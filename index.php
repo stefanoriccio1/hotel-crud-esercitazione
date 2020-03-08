748f79cfe315ffc0613312a410675555d0e41c26
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-08 16:30:46 +0100

    up

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include 'database.php';
+include 'partials/header.php';
+include 'partials/footer.php';
+?>
---
+++ partials/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="http://localhost:8888/hotel-crud-esercitazione/dist/app.css">
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
