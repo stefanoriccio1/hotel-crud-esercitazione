37770c5b22c3048f49c511cc979ecb7acf63550f
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-08 17:51:55 +0100

    up

--- partials/footer.php
+++ partials/footer.php
@@ -1,2 +1,3 @@
+    <script src="dist/app.js" charset="utf-8"></script>
   </body>
 </html>
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const $ = require('jquery');
+const Handlebars = require('handlebars');
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
