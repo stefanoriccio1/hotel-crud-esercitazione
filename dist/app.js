e3a0838174bca18bfabb72c6cea3ecaf084a4c51
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-10 15:10:46 +0100

    up

--- dist/app.js
+++ dist/app.js
@@ -15818,8 +15818,12 @@ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"
 var Handlebars = __webpack_require__(/*! handlebars */ "./node_modules/handlebars/dist/cjs/handlebars.js");
 
 $(document).ready(function () {
-  console.log("ready!");
+  $.getJSON("get_homepage.php", processRooms);
 });
+
+function processRooms(data, status, xhr) {
+  console.log(data);
+}
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -2,5 +2,10 @@ const $ = require('jquery');
 const Handlebars = require('handlebars');
 
 $( document ).ready(function() {
-    console.log( "ready!" );
+  $.getJSON("get_homepage.php", processRooms);
 });
+
+function processRooms (data, status, xhr){
+
+console.log(data);
+}
